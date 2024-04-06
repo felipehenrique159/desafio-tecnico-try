776c1bbebe225ce7f212ff24fa3fa28279606fc4
felipehenrique159 <felipecod6@hotmail.com>
2024-04-06 14:40:41 -0300

    feat: usado biblioteca sweetAlert para exibir erros de validação da api de forma dinamica

---
+++ application-vue/src/utils/errosValidacao.js
@@ -0,0 +1,14 @@
+import Swal from 'sweetalert2'
+
+function tratarMensagensErro(error) {
+    if (error && error.response.data.message == 'dados inválidos') {
+        Swal.fire({
+            title: error.response.data.message,
+            html: Object.values(error.response.data.errors).join('<br>'),
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        })
+    }
+}
+
+export default tratarMensagensErro
